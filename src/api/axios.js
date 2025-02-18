b433989bc23ac2e8c5a75d49103be831ddb34465
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2025-02-18 21:19:46 +0100

    voice

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,29 @@
-import axios from "axios";
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+  import axios from "axios";
+  const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
+  const getAccessToken = () => localStorage.getItem("accessToken");
 
-export default axios.create({
-  baseURL: BASE_URL,
-  headers: { "Content-Type": "application/json" },
-});
+  const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    headers: { "Content-Type": "application/json" },
+  });
+
+  const axiosPrivate = axios.create({
+    baseURL: BASE_URL,
+    headers: { "Content-Type": "application/json" },
+    withCredentials: true,
+  });
+
+  axiosPrivate.interceptors.request.use(
+    (config) => {
+      const token = getAccessToken();
+      if (token && !config.headers["Authorization"]) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
+
+  export default axiosInstance;
+  export { axiosPrivate };
