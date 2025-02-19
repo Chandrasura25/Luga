524578a0144db465b5471ffc7be73049af658393
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2025-02-19 01:31:58 +0100

    quota

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./components/auth.tsx";
+import { Toaster } from "./components/ui/toaster"
 import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <AuthProvider>
         <App />
+        <Toaster />
         <ToastContainer
           position="bottom-right"
           autoClose={5000}
