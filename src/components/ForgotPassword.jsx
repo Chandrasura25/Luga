aa3f5d14f68ef47e838fcaf9453e0ea9e1ddb873
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2025-02-22 22:53:01 +0100

    fixes

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -7,21 +7,28 @@ const ForgotPassword = () => {
   const [email, setEmail] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  const toast = useToast();
+  const { toast } = useToast();
 
-  const handleForgotPassword = async () => {
-    try {   
+  const handleForgotPassword = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
       const response = await axios.post("/user/forgot-password", { email });
-     if(response.status === 200){
-      navigate("/reset-code");
-     }
+      if (response.status === 200) {
+        navigate("/reset-code");
+        toast({
+          title: "Success",
+          description: "Password reset email sent, please check your email",
+          variant: "default",
+        });
+      }
     } catch (error) {
       console.error(error);
       toast({
         title: "Error",
-        description: error.response.data.message,
+        description: error.response.data.detail,
         variant: "destructive",
-      })
+      });
     } finally {
       setLoading(false);
     }
@@ -31,46 +38,46 @@ const ForgotPassword = () => {
     <div className="min-h-screen bg-white flex justify-center items-center">
       {/* container */}
       <div className="w-[400px] flex flex-col gap-4">
-      <h2 className="text-2xl font-medium text-center mb-2">
-        Welcome to Luga.ai
-      </h2>
+        <h2 className="text-2xl font-medium text-center mb-2">
+          Welcome to Luga.ai
+        </h2>
 
-      <form onSubmit={handleForgotPassword} className="space-y-4">
-        <input
-          type="email"
-          placeholder="Enter your email"
-          value={email}
-          className="w-full p-3 rounded-lg bg-gray-50"
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <div className="flex items-center justify-center">
-          <button
-            type="submit"
-            disabled={loading}
-            className="px-6 py-2.5 text-base font-medium text-white bg-black rounded-full hover:bg-gray-800 transition-colors w-full"
+        <form onSubmit={handleForgotPassword} className="space-y-4">
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            className="w-full p-3 rounded-lg bg-gray-50"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <div className="flex items-center justify-center">
+            <button
+              type="submit"
+              disabled={loading}
+              className="px-6 py-2.5 text-base font-medium text-white bg-black rounded-full hover:bg-gray-800 transition-colors w-full"
+            >
+              {loading ? "Sending..." : "Send"}
+            </button>
+          </div>
+        </form>
+        <div className="flex items-center justify-between">
+          <a
+            className="text-blue-600 hover:text-blue-800 hover:underline"
+            href="/forgot-password"
           >
-            {loading ? "Verifying..." : "Verify"}
-          </button>
+            Forgot Password?
+          </a>
+          <a
+            className="text-blue-600 hover:text-blue-800 hover:underline"
+            href="/register"
+          >
+            Register
+          </a>{" "}
         </div>
-      </form>
-      <div className="flex items-center justify-between">
-        <a
-          className="text-blue-600 hover:text-blue-800 hover:underline"
-          href="/forgot-password"
-        >
-          Forgot Password?
-        </a>
-        <a
-          className="text-blue-600 hover:text-blue-800 hover:underline"
-          href="/register"
-        >
-          Register
-        </a>{" "}
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default ForgotPassword
+export default ForgotPassword;
