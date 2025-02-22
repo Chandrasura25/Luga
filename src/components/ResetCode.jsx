3acb3af30bda735cacde2dba4ba7f68356e51e50
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2025-02-22 23:03:44 +0100

    fixes

--- src/components/ResetCode.jsx
+++ src/components/ResetCode.jsx
@@ -6,17 +6,32 @@ import {
   InputOTPSlot,
 } from "../components/ui/input-otp";
 import axios from "../api/axios";
+import { useNavigate } from "react-router-dom";
+import { useToast } from "../hooks/use-toast";
 
 const ResetCode = () => {
   const [resetCode, setResetCode] = useState("");
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { toast } = useToast();
 
   const handleResetCode = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post("/user/reset-password", { reset_code:resetCode });
-      console.log(response);
+      setLoading(true);
+      const response = await axios.post("/user/reset-password", {
+        reset_code: resetCode,
+      });
+      if (response.status === 200) {
+        localStorage.setItem("email", response.data.email);
+        navigate("/reset-password");
+      }
     } catch (error) {
+      toast({
+        title: "Error",
+        description: error.response.data.detail,
+        variant: "destructive",
+      });
       console.error(error);
     } finally {
       setLoading(false);
@@ -27,9 +42,7 @@ const ResetCode = () => {
     <div className="min-h-screen bg-white flex justify-center items-center">
       {/* container */}
       <div className="max-w-[350px] flex flex-col gap-4">
-        <h2 className="text-2xl font-medium text-center">
-          Welcome to Luga.ai
-        </h2>
+        <h2 className="text-2xl font-medium text-center">Welcome to Luga.ai</h2>
         <p className="text-sm text-center mb-4">
           Enter the code sent to your email
         </p>
