6f6f5aa8083eab457c71086eb1287625dab717a6
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2025-04-01 09:39:59 +0100

    fixes

---
+++ app/db/models.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+from typing import Optional, List
+from datetime import datetime
+
+class TextCreate(BaseModel):
+    prompt: str
+    user_email: str
+
+class TextResponse(BaseModel):
+    prompt: str
+    response: str
+    timestamp: Optional[str] = None
+    date: Optional[str] = None
+    conversation_id: Optional[str] = None
+    title: Optional[str] = None
+    warning: Optional[str] = None
+
+class ConversationCreate(BaseModel):
+    prompt: str
+    user_email: str
+    conversation_id: Optional[str] = None
+    title: Optional[str] = None
+
+class Message(BaseModel):
+    prompt: str
+    response: str
+    timestamp: str
+
+class ConversationResponse(BaseModel):
+    conversation_id: str
+    title: str
+    messages: List[Message] 
